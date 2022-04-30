dedbaf5f5bbea22f7d2fadd645d9854232b4159b
butterchoke <rohanrwa@gmail.com>
2022-04-30 15:29:59 +0100

    I made this part

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+  system("clear");
+  printf("████████╗██╗    ██╗███████╗███████╗████████╗███████╗██████╗ \n");
+  printf("╚══██╔══╝██║    ██║██╔════╝██╔════╝╚══██╔══╝██╔════╝██╔══██╗ \n");
+  printf("   ██║   ██║ █╗ ██║█████╗  █████╗     ██║   █████╗  ██████╔╝ \n");
+  printf("   ██║   ██║███╗██║██╔══╝  ██╔══╝     ██║   ██╔══╝  ██╔══██╗ \n");
+  printf("   ██║   ╚███╔███╔╝███████╗███████╗   ██║   ███████╗██║  ██║ \n");
+  printf("   ╚═╝    ╚══╝╚══╝ ╚══════╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝ \n");
+
+  printf("\nBy Aiden Simmons and Rohan Bhattacharya\n\n\n\nPress enter to continue.\n");
+  char input;
+  scanf("%c",&input);
+  system("clear");
+
+  printf("HELLO\n");
+  scanf("%c",&input);
+
+
+  }

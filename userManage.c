c7078c48a483850da54244c531d0a9779ca78e08
Aiden Simmons <aidentsimmons@gmail.com>
2022-04-30 19:30:44 +0100

    Minor restructuring

--- userManage.c
+++ userManage.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
 
 int getNumUsers(){
     int numUsers;
     printf("How many users would you like to have?\n");
     scanf("%d",&numUsers);
     return numUsers;
+}
+
+char generateUsername(){
+    
 }

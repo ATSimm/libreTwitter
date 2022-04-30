4b525ebfe14680544ab837cc223831fb59275b1d
butterchoke <rohanrwa@gmail.com>
2022-04-30 20:28:56 +0100

    Made adjustments to structures and hash

--- tweetManage.c
+++ tweetManage.c
@@ -3,7 +3,12 @@
 #include <string.h>
 #include <stdbool.h>
 #include "linkedList.c"
+#include "structures.c"
+#include "hash.c"
 
 void postTweet(){
-    
+    struct tweet post;
+    post.id = hash();
+    post.author = "";
+    post.content = "";
 }

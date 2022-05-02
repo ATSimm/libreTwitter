6caf5e46f74bfec42117b89e840b55654328d069
butterchoke <rohanrwa@gmail.com>
2022-05-02 18:23:13 +0100

    a

--- test.c
+++ test.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "linkedList.c"
-//#include "userManage.c"
+//#include "linkedList.c"
+#include "userManage.c"
 
 int main(){
+  /*
   insertBegin(1,"This is the content that I am inserting");
   insertBegin(1,"This is the content that I am inserting");
   traverse();
+  */
+ generateUsername();
 }

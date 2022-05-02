c6a45b44a6c3ed3ba623f1911bdb153475c02772
Supernovae <56899031+ATSimm@users.noreply.github.com>
2022-05-02 18:24:14 +0100

    Initialized follower management program

---
+++ followerManage.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+char *followers(int numUsers, char[] currentUser){
+
+    char *followers[numUsers][numUsers]; //first dimension identifies the user, second dimension identifies followers
+
+
+    for (i = 0; i < numUsers - 1; i++){
+        followers[currentUser][i]
+    }
+}

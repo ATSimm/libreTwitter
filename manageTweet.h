7ed4a70afa7afb27354ddaa45fed0d4734da1b90
Supernovae <56899031+ATSimm@users.noreply.github.com>
2022-05-05 19:42:12 +0100

    minor bug fix

--- manageTweet.h
+++ manageTweet.h
@@ -1,5 +1,6 @@
 #ifndef LIBRETWITTER_MANAGETWEET_H
 #define LIBRETWITTER_MANAGETWEET_H
+#include "structures.h"
 
 void postTweet(int id, char *content, char *authorName);
 void showFeed(struct user currentPoster, int numUsers);

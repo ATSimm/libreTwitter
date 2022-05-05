fcaffc3c13b092b045743ce887a1c73df137d119
butterchoke <rohanrwa@gmail.com>
2022-05-05 18:38:08 +0100

    Post and view tweet

--- manageTweet.h
+++ manageTweet.h
@@ -2,8 +2,8 @@
 #define LIBRETWITTER_MANAGETWEET_H
 
 
-void insert(int id, char *content, char *authorName);
-void traverse(bool **followArray, int currentUser, int numUsers);
+void postTweet(int id, char *content, char *authorName);
+void showFeed(bool **followArray, int currentUser, int numUsers);
 
 
 #endif //LIBRETWITTER_MANAGETWEET_H
